5910a8117405cf6a56b3ccf63a0c40d4015d1ed4
Tali Ngatuvai <tali_ngatuvai@Talis-MacBook-Pro.local>
2019-09-28 19:41:53 -0600

    alerts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     this.state.cards.forEach(card => {
       card.count = 0;
     });
-    alert(` \nscore: ${this.state.score}`);
+    alert(`Loser!! \nscore: ${this.state.score}`);
     this.setState({ score: 0 });
     return true;
   }
